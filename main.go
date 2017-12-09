635ff8fd0fbd9271c0306499c38d8f8d4c18c519
Geoff Smith <gsmith@bluetarp.com>
2017-12-09 09:07:27 -0500

    pulled routes out of main()

--- main.go
+++ main.go
@@ -2,31 +2,14 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
-
-	"github.com/smithgeoffrey/go-coop/api"
-	"github.com/smithgeoffrey/go-coop/ui"
 )
 
 func main() {
 	router := gin.Default()
 	router.LoadHTMLGlob("ui/templates/*")
 
-	r1 := router.Group("/api/v1")
-	{
-		r1.GET("/sensor", api.ListSensors)
-		r1.GET("/sensor/door", api.ListDoorSensors)
-		r1.GET("/sensor/door/:id", api.GetDoorSensor)
-		r1.GET("/sensor/temp", api.ListTempSensors)
-		r1.GET("/sensor/temp/:id", api.GetTempSensor)
-		r1.GET("/video", api.ListVideo)
-		r1.GET("/video/:id", api.GetVideo)
-	}
-
-	r2 := router.Group("")
-	{
-		r2.GET("/", ui.Home)
-		r2.GET("/param/:name", ui.Param)
-	}
+	// from ~/routes.go
+	initializeRoutes(router)
 
 	router.Run(":8080")
 }
---
+++ routes.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+
+	"github.com/smithgeoffrey/go-coop/api"
+	"github.com/smithgeoffrey/go-coop/ui"
+)
+
+func initializeRoutes(router *gin.Engine) {
+
+	r1 := router.Group("/api/v1")
+	{
+		r1.GET("/sensor", api.ListSensors)
+		r1.GET("/sensor/door", api.ListDoorSensors)
+		r1.GET("/sensor/door/:id", api.GetDoorSensor)
+		r1.GET("/sensor/temp", api.ListTempSensors)
+		r1.GET("/sensor/temp/:id", api.GetTempSensor)
+		r1.GET("/video", api.ListVideo)
+		r1.GET("/video/:id", api.GetVideo)
+	}
+
+	r2 := router.Group("")
+	{
+		r2.GET("/", ui.Home)
+		r2.GET("/param/:name", ui.Param)
+	}
+
+}
