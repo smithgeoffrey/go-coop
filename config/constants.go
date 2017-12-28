d1ea60b0daa5c189bad02786bac4e1998a29b7d9
Geoff Smith <gsmith@bluetarp.com>
2017-12-28 17:17:11 -0500

    bug fix after port change

--- config/constants.go
+++ config/constants.go
@@ -2,8 +2,8 @@ package config
 
 const (
 	VIDEO_URL = "http://172.16.1.128/video.mjpg"
-	TEMP_URL = "http://localhost:8080/api/v1/sensor/temp"
-	DOOR_URL = "http://localhost:8080/api/v1/sensor/door"
+	TEMP_URL = "http://localhost:8081/api/v1/sensor/temp"
+	DOOR_URL = "http://localhost:8081/api/v1/sensor/door"
 
 	MOCK_TEMP_INSIDE_SENSOR = "35.1"
 	MOCK_TEMP_OUTSIDE_SENSOR = "28.4"
