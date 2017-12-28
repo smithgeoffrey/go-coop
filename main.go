8c1a6b36f17daf17b0209c264c56c31d4581a504
Geoff Smith <gsmith@bluetarp.com>
2017-12-28 16:59:15 -0500

    bug fix re templates glob

--- main.go
+++ main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"os"
+	"path/filepath"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	router := gin.Default()
-	router.LoadHTMLGlob("ui/templates/*")
+	router.LoadHTMLGlob(filepath.Join(os.Getenv("GOPATH"),
+		"src/github.com/smithgeoffrey/go-coop/ui/templates/*"))
 
 	// from ~/routes.go
 	initializeRoutes(router)
